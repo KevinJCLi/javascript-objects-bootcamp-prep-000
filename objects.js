cde73e68684727b14770e3724a785d135b3d6e3e
Kevin Li <kevinjcli@hotmail.com>
2018-02-02 18:12:10 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {"Avril Lavigne": "Nobody's Home"};
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  var newPlaylist = {};
+  newPlaylist = Object.assign(newPlaylist, playlist, {[artistName]: songTitle});
+  return newPlaylist;
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
+
